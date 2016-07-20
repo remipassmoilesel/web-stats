69aeb436225149235a379d2677fccaaa4fada1ac
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-07-20 10:21:15 +0200

    Add last events components

--- public/src/scripts/visualization.js
+++ public/src/scripts/visualization.js
@@ -10,6 +10,7 @@ require("./components/timeline/timeline.js")(visualizationModule);
 require("./components/pie-chart/pie-chart.js")(visualizationModule);
 require("./components/event-list/event-list.js")(visualizationModule);
 require("./components/event-resume/event-resume.js")(visualizationModule);
+require("./components/last-events/last-events.js")(visualizationModule);
 
 $(function() {
 
